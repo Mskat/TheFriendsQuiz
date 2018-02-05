5a306931ec8a3cacd2c8f6eb7e13ec7f6a75ee51
mskatz <zur.katarzyna@gmail.com>
2018-02-05 23:03:35 +0100

    Change color of texts, buttons, background, actionbar

--- app/src/main/java/com/example/mskatz/friendsquiz/MainActivity.java
+++ app/src/main/java/com/example/mskatz/friendsquiz/MainActivity.java
@@ -12,7 +12,7 @@ import static android.icu.lang.UCharacter.toUpperCase;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score =0;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,72 +39,73 @@ public class MainActivity extends AppCompatActivity {
         if (sixthAnswer()) {
             addPoint(score);
         }
-        if(score==6){
+        if (score == 6) {
             Toast.makeText(this, "Your score is " + String.valueOf(score) + "\nCongratulations!", Toast.LENGTH_SHORT).show();
-        } else Toast.makeText(this, "Your score is " + String.valueOf(score) + "\nNext time you will be better :)", Toast.LENGTH_SHORT).show();
+        } else
+            Toast.makeText(this, "Your score is " + String.valueOf(score) + "\nNext time you will be better :)", Toast.LENGTH_SHORT).show();
 
         score = 0;
     }
 
-    public void addPoint(int point){
+    public void addPoint(int point) {
         score += 1;
     }
 
-    private String firstAnswer(){
+    private String firstAnswer() {
         EditText editText = findViewById(R.id.place_of_work);
         String placeOfWork = editText.getText().toString();
         return placeOfWork;
     }
 
-    private boolean secondAnswer(){
+    private boolean secondAnswer() {
         boolean trueOrFalse = false;
         CheckBox stateChick = findViewById(R.id.chick);
         CheckBox stateDog = findViewById(R.id.dog);
         CheckBox stateDuck = findViewById(R.id.duck);
         CheckBox stateMonkey = findViewById(R.id.monkey);
-        if(stateDuck.isChecked() && stateChick.isChecked()) {
+        if (stateDuck.isChecked() && stateChick.isChecked()) {
             trueOrFalse = true;
-            if(stateDog.isChecked() || stateMonkey.isChecked()){
+            if (stateDog.isChecked() || stateMonkey.isChecked()) {
                 trueOrFalse = false;
             }
         }
         return trueOrFalse;
     }
 
-    private boolean thirdAnswer(){
+    private boolean thirdAnswer() {
         boolean trueOrFalse = false;
         RadioButton radioButton = findViewById(R.id.answer_no);
-        if(radioButton.isChecked()){
+        if (radioButton.isChecked()) {
             trueOrFalse = true;
         }
         return trueOrFalse;
     }
 
-    private String fourthAnswer(){
+    private String fourthAnswer() {
         EditText editText = findViewById(R.id.name_of_town);
         String nameOfTown = editText.getText().toString();
         return nameOfTown;
     }
 
-    private boolean fifthAnswer(){
+    private boolean fifthAnswer() {
         boolean trueOrFalse = false;
         CheckBox stateChandler = findViewById(R.id.chandler);
         CheckBox statePhoebe = findViewById(R.id.phoebe);
         CheckBox stateRachel = findViewById(R.id.rachel);
         CheckBox stateJoey = findViewById(R.id.joey);
-        if(stateRachel.isChecked() && stateJoey.isChecked()) {
+        if (stateRachel.isChecked() && stateJoey.isChecked()) {
             trueOrFalse = true;
-            if(stateChandler.isChecked() || statePhoebe.isChecked()){
+            if (stateChandler.isChecked() || statePhoebe.isChecked()) {
                 trueOrFalse = false;
             }
         }
         return trueOrFalse;
     }
 
-    private boolean sixthAnswer(){
+    private boolean sixthAnswer() {
         boolean trueOrFalse = false;
         RadioButton radioButton = findViewById(R.id.answer_muriel);
-        if(radioButton.isChecked()){
+        if (radioButton.isChecked()) {
             trueOrFalse = true;
         }
         return trueOrFalse;
