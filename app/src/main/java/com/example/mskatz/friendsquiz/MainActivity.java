7caab781c61727586dc15bddb1d629a47b0c6dfb
mskatz <zur.katarzyna@gmail.com>
2018-03-20 21:30:03 +0100

    Modified six methods that checks is the answer correct or not, added comments

--- app/src/main/java/com/example/mskatz/friendsquiz/MainActivity.java
+++ app/src/main/java/com/example/mskatz/friendsquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mskatz.friendsquiz;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +9,13 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import static android.icu.lang.UCharacter.toUpperCase;
+/**
+ * This app tests knowledge from tv show The Friends in the form of a quiz
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    //general value of a score
     int score = 0;
 
     @Override
@@ -20,25 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Checks the user's response, sums up the result and displays a message containing the result
+     *
+     */
     public void result(View view) {
-        if (toUpperCase(firstAnswer()).equals(getString(R.string.central_perk))) {
-            addPoint(score);
-        }
-        if (secondAnswer()) {
-            addPoint(score);
-        }
-        if (thirdAnswer()) {
-            addPoint(score);
-        }
-        if (toUpperCase(fourthAnswer()).equals(getString(R.string.tulsa))) {
-            addPoint(score);
-        }
-        if (fifthAnswer()) {
-            addPoint(score);
-        }
-        if (sixthAnswer()) {
-            addPoint(score);
-        }
+        firstAnswer();
+        secondAnswer();
+        thirdAnswer();
+        fourthAnswer();
+        fifthAnswer();
+        sixthAnswer();
+
         if (score == 6) {
             Toast.makeText(this, getString(R.string.score) + " " + String.valueOf(score) + "\n" + getString(R.string.congratulations), Toast.LENGTH_SHORT).show();
         } else
@@ -47,61 +44,121 @@ public class MainActivity extends AppCompatActivity {
         score = 0;
     }
 
+    /**
+     * Adds one point to general score
+     *
+     */
     public void addPoint(int point) {
         score += 1;
     }
 
-    private String firstAnswer() {
-        EditText editText = findViewById(R.id.place_of_work);
-        return editText.getText().toString();
+    /**
+     * Changes the background color to green, for the correct answer
+     *
+     */
+    private void changeColorC(View v) {
+        v.setBackgroundColor(Color.parseColor("#009900"));
     }
 
-    private boolean secondAnswer() {
-        boolean trueOrFalse = false;
+    /**
+     * Changes the background color to red, for the incorrect answer
+     */
+    private void changeColorW(View v) {
+        v.setBackgroundColor(Color.RED);
+    }
+
+    /**
+     * Answer for the first question: Where does \'Gunther\' work?
+     * This method checks is user's answer correct or not
+     */
+    private void firstAnswer() {
+        EditText editText = findViewById(R.id.place_of_work);
+        if (editText.getText().toString().toUpperCase().equals(getString(R.string.central_perk))) {
+            addPoint(score);
+            changeColorC(editText);
+        } else {
+            changeColorW(editText);
+        }
+    }
+
+    /**
+     * Answer for the second question: What types of an animal do Joey and Chandler own?
+     * This method checks is user's answer correct or not
+     */
+    private void secondAnswer() {
         CheckBox stateChick = findViewById(R.id.chick);
         CheckBox stateDog = findViewById(R.id.dog);
         CheckBox stateDuck = findViewById(R.id.duck);
         CheckBox stateMonkey = findViewById(R.id.monkey);
-        if (stateDuck.isChecked() && stateChick.isChecked()) {
-            trueOrFalse = !(stateDog.isChecked() || stateMonkey.isChecked());
+        if (stateDuck.isChecked() && stateChick.isChecked() && !(stateDog.isChecked()) && !(stateMonkey.isChecked())) {
+            addPoint(score);
+            changeColorC(stateChick);
+            changeColorC(stateDuck);
+        } else {
+            changeColorW(stateChick);
+            changeColorW(stateDuck);
         }
-        return trueOrFalse;
     }
 
-    private boolean thirdAnswer() {
-        boolean trueOrFalse = false;
+    /**
+     * Answer for the third question: Did Phoebe carry for her brother two babies?
+     * This method checks is user's answer correct or not
+     */
+    private void thirdAnswer() {
         RadioButton radioButton = findViewById(R.id.answer_no);
         if (radioButton.isChecked()) {
-            trueOrFalse = true;
+            addPoint(score);
+            changeColorC(radioButton);
+        } else {
+            changeColorW(radioButton);
         }
-        return trueOrFalse;
     }
 
-    private String fourthAnswer() {
+    /**
+     * Answer for the fourth question: Where is Chandler forced to work after falling asleep in a meeting?
+     * This method checks is user's answer correct or not
+     */
+    private void fourthAnswer() {
         EditText editText = findViewById(R.id.name_of_town);
-        return editText.getText().toString();
-
+        if (editText.getText().toString().toUpperCase().equals(getString(R.string.tulsa))) {
+            addPoint(score);
+            changeColorC(editText);
+        } else {
+            changeColorW(editText);
+        }
     }
 
-    private boolean fifthAnswer() {
-        boolean trueOrFalse = false;
+    /**
+     * Answer for the fifth question: Which of The Friends have worked in the coffee shop?
+     * This method checks is user's answer correct or not
+     */
+    private void fifthAnswer() {
         CheckBox stateChandler = findViewById(R.id.chandler);
         CheckBox statePhoebe = findViewById(R.id.phoebe);
         CheckBox stateRachel = findViewById(R.id.rachel);
         CheckBox stateJoey = findViewById(R.id.joey);
-        if (stateRachel.isChecked() && stateJoey.isChecked()) {
-            trueOrFalse = !(stateChandler.isChecked() || statePhoebe.isChecked());
+        if (stateRachel.isChecked() && stateJoey.isChecked() && !(stateChandler.isChecked()) && !(statePhoebe.isChecked())) {
+            addPoint(score);
+            changeColorC(stateRachel);
+            changeColorC(stateJoey);
+        } else {
+            changeColorW(stateRachel);
+            changeColorW(stateJoey);
         }
-        return trueOrFalse;
     }
 
-    private boolean sixthAnswer() {
-        boolean trueOrFalse = false;
+    /**
+     * Answer for the sixth question: What is Chandler's middle name?
+     * This method checks is user's answer correct or not
+     */
+    private void sixthAnswer() {
         RadioButton radioButton = findViewById(R.id.answer_muriel);
         if (radioButton.isChecked()) {
-            trueOrFalse = true;
+            addPoint(score);
+            changeColorC(radioButton);
+        } else {
+            changeColorW(radioButton);
         }
-        return trueOrFalse;
     }
 }
 
